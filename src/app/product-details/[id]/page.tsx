4851d0b6a1570ea0115671de7d782066b1914be0
Mazhar Hussain <mianmazhar301@gmail.com>
2025-11-19 23:13:11 +0500

    Update API endpoint for fetching cloth details

--- src/app/product-details/[id]/page.tsx
+++ src/app/product-details/[id]/page.tsx
@@ -12,7 +12,7 @@ export default async function ProductDetailPage({ params }: Props) {
 
 
   const res = await fetch(
-    `https://mian-cloth-house-next-js.vercel.app/api/cloths?id=${id}`,
+    `https://mian-cloth-house-next-5j0h52txp.vercel.app/api/cloths?id=${id}`,
     {
       cache: "force-cache", 
     }
